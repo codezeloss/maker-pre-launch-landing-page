75845da973733be3d88774375561bf4731b0b4ac
Elhoussaine AIT AICHTE <elhoussaineaitX@gmail.com>
2022-05-22 17:23:15 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-"use strict";
+'use strict';
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const errorMessage = document.querySelector('.error-message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (email.value === '') {
+    email.classList.add('invalid-input');
+    errorMessage.classList.remove('hidden');
+  } else {
+    email.classList.add('valid-input');
+    errorMessage.classList.add('hidden');
+  }
+});
